f4499eaf879b1c6c5978176b8238dae1d33bed67
Aleksandar Savchev <aleksandar.savchev@sap.com>
2026-03-13 11:19:17 +0200

    make generate

--- internal/reconciler/compliancescan/reconciler_test.go
+++ internal/reconciler/compliancescan/reconciler_test.go
@@ -16,6 +16,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -29,7 +30,6 @@ import (
 	configv1alpha1 "github.com/gardener/diki-operator/pkg/apis/config/v1alpha1"
 	dikiinstall "github.com/gardener/diki-operator/pkg/apis/diki/install"
 	dikiv1alpha1 "github.com/gardener/diki-operator/pkg/apis/diki/v1alpha1"
-	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 )
 
 var _ = Describe("Controller", func() {
